c601f03f505b1558c22cafd52f7123a160f3d722
Yurii Tsiupa <zver.enigma98@gmail.com>
2021-08-17 17:53:21 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import {ChatEngine} from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import Modal from "./components/LoginForm";
+
 import './App.css';
 
-function App() {
+const projectID = "cfba254d-5a0e-492d-a0b4-57974d510e2f";
+
+const App = () => {
+
+  if (!localStorage.getItem("username")) return <Modal/>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ChatEngine
+    height="100vh"
+    projectID={projectID}
+    userName={localStorage.getItem("username")}
+    userSecret={localStorage.getItem("password")}
+    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+  />    
   );
 }
 
